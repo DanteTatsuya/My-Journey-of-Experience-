0b99b37e6aaa5650269c437c1787d7ae3f225a20
Rakhmonjon Karimov <karimovrr23@gmail.com>
2024-02-12 01:55:58 -0500

    Hexagon!

---
+++ Drawing.py
@@ -0,0 +1,9 @@
+#Rakhmonjon Karimov
+#February 12, 2024
+#This is code draws a hexagon that is 6 sided!
+
+import turtle
+dante = turtle.Turtle()
+for i in range(6):
+    dante.forward(100)
+    dante.right(360/6)
