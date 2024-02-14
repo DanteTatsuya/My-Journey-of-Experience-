9afd9d5a4d65612a6a38efea0e735f479d808054
Rakhmonjon Karimov <karimovrr23@gmail.com>
2024-02-13 22:22:48 -0500

    5-Star

---
+++ Drawing p3.py
@@ -0,0 +1,10 @@
+#Rakhmonjon Karimov
+#This is a drawing of a 5-pointed star!
+
+import turtle
+
+dante = turtle.Turtle()
+for i in range(5):
+    dante.forward(100)
+    dante.right(144)
+turtle.done()
